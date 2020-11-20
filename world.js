79a61fce0202d69ff27820bcb576fc37c06a3040
tyreke-mclean <tyrekemclean@gmail.com>
2020-11-19 21:21:51 -0500

    La Perfecion world.js

--- world.js
+++ world.js
@@ -1,25 +1,39 @@
 document.addEventListener('DOMContentLoaded', function(){
-    document.getElementById("searchBtn").addEventListener('click',(event) => {
+    
+  document.getElementById("lookup").addEventListener('click',(event) => {
         event.preventDefault();
+        xmlhttpreq=new XMLHttpRequest();
         
-        if (window.XMLHttpRequest) {
-            xmlhttpreq=new XMLHttpRequest();
-          } else { 
-            xmlhttpreq=new ActiveXObject("Microsoft.XMLHTTP");
-          }
-          xmlhttpreq.onreadystatechange=function() { 
-            if (this.readyState==4 && this.status==200) {
-              
-               //document.getElementById("result").innerHTML=this.responseText;
-               alert(this.responseText); 
-            }
-          }
+        xmlhttpreq.onreadystatechange = function(){
+        if(xmlhttpreq.readyState===XMLHttpRequest.DONE && xmlhttpreq.status===200){
+          document.getElementById("result").innerHTML=xmlhttpreq.responseText;
+        }
+        if( xmlhttpreq.readyState===XMLHttpRequest.DONE && xmlhttpreq.status===404){
+          alert("ERROR:");
+        }
           
-          xmlhttpreq.open("GET","http://localhost:8080/world.php?q="+(document.getElementById("search box").value),true);
+      }
+          xmlhttpreq.open("GET","http://localhost/info2180-lab5/world.php?country="+(document.getElementById("country").value),true);
           xmlhttpreq.send();
 
     });
     
-    
+    document.getElementById("lookupCities").addEventListener('click',(event) => {
+      event.preventDefault();
+      xmlhttpreq=new XMLHttpRequest();
+
+      xmlhttpreq.onreadystatechange = function(){
+        if(xmlhttpreq.readyState===XMLHttpRequest.DONE && xmlhttpreq.status===200){
+          document.getElementById("result").innerHTML=xmlhttpreq.responseText;
+        }
+        if( xmlhttpreq.readyState===XMLHttpRequest.DONE && xmlhttpreq.status===404){
+          alert("ERROR:");
+        }
+          
+      }
+      xmlhttpreq.open("GET","http://localhost/info2180-lab5/world.php?country="+(document.getElementById("country").value)+"city=cities",true);;
+        xmlhttpreq.send();
+
+  });
     
 }, false);
